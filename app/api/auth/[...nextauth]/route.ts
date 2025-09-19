0ed231c682470fadb994408902212e5986287e67
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-19 21:07:22 +0530

    created  wallet callback

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { db } from "@/app/db"
 
 const handler = NextAuth({
     providers: [
@@ -13,10 +14,36 @@ const handler = NextAuth({
         async signIn({ user, account, profile }) {
             if (account?.provier != 'google') {
                 const email = user.email
-                if (!email) return false;
+                if (!email) {
+                    return false
+                }
+
+                const userDB = await db.user.findFirst({
+                    where: {
+                        username: email
+                    }
+                })
+
+                if (userDB) return true
+
+                await db.user.create({
+                    data: {
+                        username: email,
+                        provider: "Google",
+                        solanaWallet: {
+                            create: {
+                                pubkey: "",
+                                privateKey: ""
+                            }
+                        },
+                        inrWallet: {
+                            create: {
+                                balance: 0
+                            }
+                        }
+                    }
+                })
             }
-            console.log({ user, account, profile })
-            return true
         }
     }
 })
