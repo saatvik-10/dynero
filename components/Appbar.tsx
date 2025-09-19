563d9b060fd6a850daaa069c9afbb39a99c3fb0b
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-19 18:54:14 +0530

    updated appbar

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -9,13 +9,13 @@ const Appbar = () => {
 
   return (
     <div className='flex flex-row max-w-7xl shadow-lg border-none items-center justify-between p-4 m-6 rounded-md'>
-      <div>DYNERO</div>
+      <div className='font-bold'>DYNERO</div>
 
       <div>
         {session?.data?.user ? (
-          <Button onClick={() => signOut}>Sign Out</Button>
+          <Button className='bg-blue-500 cursor-pointer hover:bg-blue-500/80' onClick={() => signOut}>Sign Out</Button>
         ) : (
-          <Button onClick={() => signIn}>Sign In</Button>
+          <Button className='bg-red-500 cursor-pointer hover:bg-red-500/80' onClick={() => signIn}>Sign In</Button>
         )}
       </div>
     </div>
