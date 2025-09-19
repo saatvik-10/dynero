aaa241d425db199865f5d4bdbcdd4d5c61872f36
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-19 20:09:58 +0530

    updated btn states with sonner

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -3,9 +3,27 @@
 import React from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { Button } from './ui/button';
+import { toast } from 'sonner';
 
 const Appbar = () => {
   const session = useSession();
+
+  const handleSignIn = async() => {
+    try {
+      await signIn('google');
+    } catch (err:any) {
+      toast.error("Error signing in", err)
+    }
+  }
+
+  const handleSignOut = async() => {
+    try {
+      await signOut();
+      toast.success("Signed out successfully")
+    } catch (err: any) {
+      toast.error("Error signing out", err)
+    }
+  }
 
   return (
     <div className='flex flex-row max-w-7xl shadow-lg border-none items-center justify-between p-4 m-6 rounded-md'>
@@ -13,9 +31,9 @@ const Appbar = () => {
 
       <div>
         {session?.data?.user ? (
-          <Button className='bg-red-500 cursor-pointer hover:bg-red-500/80' onClick={() => signOut}>Sign Out</Button>
+          <Button className='bg-red-500 cursor-pointer hover:bg-red-500/80' onClick={handleSignOut}>Sign Out</Button>
         ) : (
-          <Button className='bg-blue-500 cursor-pointer hover:bg-blue-500/80' onClick={() => signIn}>Sign In</Button>
+          <Button className='bg-blue-500 cursor-pointer hover:bg-blue-500/80' onClick={handleSignIn}>Sign In</Button>
         )}
       </div>
     </div>
