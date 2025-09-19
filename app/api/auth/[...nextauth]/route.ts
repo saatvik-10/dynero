11f42da908ffe2aeb009eb2f4881fc22cf8d973e
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-19 20:20:03 +0530

    added callback verification

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,26 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-}
+    callbacks: {
+        //@ts-ignore
+        async signIn({ user, account, profile }) {
+            if (account?.provier != 'google') {
+                const email = user.email
+                if (!email) return false;
+            }
+            console.log({ user, account, profile })
+            return true
+        }
+    }
+})
 
-const handler = NextAuth(authOptions)
+// const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST } 
