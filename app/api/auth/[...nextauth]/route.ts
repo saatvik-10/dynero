980bee2444bfc218a2ec88ed781a8efd6c6bf040
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-20 18:13:23 +0530

    fixed auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,6 @@
+import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { db } from "@/app/db";
-import { Keypair } from "@solana/web3.js";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    //@ts-ignore
-    async signIn({ user, account, profile }) {
-      if (account?.provier != "google") {
-        const email = user.email;
-        if (!email) {
-          return false;
-        }
-
-        const userDB = await db.user.findFirst({
-          where: {
-            username: email,
-          },
-        });
-
-        if (userDB) return true;
-
-        const keypair = Keypair.generate();
-        const publicKey = keypair.publicKey.toBase58();
-        const privateKey = keypair.secretKey;
-
-        await db.user.create({
-          data: {
-            username: email,
-            name: profile?.name,
-            //@ts-ignore
-            profilePicture: profile?.picture,
-            provider: "Google",
-            solanaWallet: {
-              create: {
-                pubkey: "",
-                privateKey: "",
-              },
-            },
-            inrWallet: {
-              create: {
-                balance: 0,
-              },
-            },
-          },
-        });
-        return true;
-      }
-      return false;
-    },
-  },
-});
-
-// const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
