726db5aac9820aaa94023f08ea6432af98ea8f54
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-20 18:14:05 +0530

    added asset section

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,19 +1,37 @@
 'use client';
 
-interface ProfileProps {
-  loading: boolean;
-  image: string;
-  name: string;
-  user: boolean;
-}
-
+import { useEffect, useState } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Separator } from '@/components/ui/separator';
 import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import { toast } from 'sonner';
+import { BadgeDollarSign, CheckCircle } from 'lucide-react';
+import { Button } from './ui/button';
 
-export function Profile({ loading, image, name, user }: ProfileProps) {
+export function Profile({ publicKey }: { publicKey: string }) {
   const route = useRouter();
+  const session = useSession();
+  const loading = session?.status == 'loading';
+
+  const [copy, setCopy] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (copy) {
+      let timeout = setTimeout(() => {
+        setCopy(false);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [copy]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(publicKey);
+    setCopy(true);
+  };
 
   if (loading) {
     return (
@@ -31,7 +49,7 @@ export function Profile({ loading, image, name, user }: ProfileProps) {
     );
   }
 
-  if (!user) {
+  if (!session?.data?.user) {
     route.push('/');
     toast.error('Please sign in to continue!');
   }
@@ -41,10 +59,40 @@ export function Profile({ loading, image, name, user }: ProfileProps) {
       <div className='w-full max-w-md rounded-md shadow-md md:max-w-xl'>
         <div className='p-10'>
           <div className='flex items-center gap-3'>
-            <img src={image} alt='' className='size-10 rounded-full' />
-            <span className='md:text-xl'>Yello again, {name}!</span>
+            <img
+              src={session?.data?.user?.image || ''}
+              alt=''
+              className='size-10 rounded-full'
+            />
+            <span className='md:text-xl'>
+              Yello again, {session?.data?.user?.name}!
+            </span>
           </div>
+
           <Separator className='my-3' />
+
+          <div className='flex flex-col'>
+            <span className='text-sm text-slate-400 flex items-center gap-1'>
+              <BadgeDollarSign className='size-4' /> Your Assets in Dynero
+            </span>
+            <div className='flex items-center justify-between pt-5'>
+              <div></div>
+              <Button
+                onClick={handleCopy}
+                size={'sm'}
+                className='bg-gradient-to-r from-blue-600 to-purple-600 cursor-pointer'
+              >
+                {copy ? (
+                  <div className='flex items-center gap-2'>
+                    <CheckCircle className='size-4 text-white' />
+                    Wallet Address copied
+                  </div>
+                ) : (
+                  'Copy Wallet Address'
+                )}
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
