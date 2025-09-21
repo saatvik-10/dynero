cd38caf2fce34db668ada5bfb22ae054e915ca00
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-21 17:18:58 +0530

    updated api

--- hooks/useToken.ts
+++ hooks/useToken.ts
@@ -2,27 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { TokenInterface } from "@/lib/token";
 
-
-interface TokenBalanceInterface extends TokenInterface {
+export interface TokenBalanceInterface extends TokenInterface {
     balance: string,
-    useBalance: string
+    usdBalance: string
 }
 
 export function useTokens(address: string) {
     const [tokenBalance, setTokenBalance] = useState<{
         totalBalance: string,
-        tokens: TokenBalanceInterface[]
+        tks: TokenBalanceInterface[]
     } | null>(null)
     const [loading, setLoading] = useState<boolean>(false)
 
     useEffect(() => {
         try {
+            setLoading(true);
             axios.get(`/api/tokens?address=${address}`)
                 .then(res =>
                     setTokenBalance(res.data)
                 )
         } catch (err) {
             console.error("failed to fetch tokens", err);
+        } finally {
+            setLoading(false)
         }
     }, [])
 
