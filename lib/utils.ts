536c71b8859f311d56ce6f5124292d44fce1b2cf
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-21 16:10:15 +0530

    ata helper fn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,31 @@
+import { getAssociatedTokenAddress, getAccount } from "@solana/spl-token";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { connection } from "./constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function getAccountBalance(tk: {
+  name: string,
+  mint: string,
+  native: boolean,
+  decimals: number
+}, address: string) {
+  if (tk.native) {
+    let balance = await connection.getBalance(new PublicKey(address));
+    console.log("balance is " + balance)
+    return balance / LAMPORTS_PER_SOL;
+  }
+
+  const ata = await getAssociatedTokenAddress(new PublicKey(tk.mint), new PublicKey(address));
+
+  try {
+    const account = await getAccount(connection, ata);
+    return Number(account.amount) / (10 ** tk.decimals)
+  } catch (err: any) {
+    return 0;
+  }
 }
