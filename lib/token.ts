660a1b7154f890c2b02d226402d50ae980df9619
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-21 16:10:08 +0530

    token data

---
+++ lib/token.ts
@@ -0,0 +1,31 @@
+interface TokenInterface {
+    name: string,
+    mint: string,
+    native: boolean,
+    price: string,
+    image: string,
+    decimals: number
+}
+
+export const SUPPORTED_TOKENS: TokenInterface[] = [{
+    name: "SOL",
+    mint: "So11111111111111111111111111111111111111112",
+    native: true,
+    price: '240',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Solana_cryptocurrency_two.jpg',
+    decimals: 9
+}, {
+    name: "USDT",
+    mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+    native: false,
+    price: '1',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvSxrpym7ij1Hf6zQOltcDORlrJGyj1kPf3A&s",
+    decimals: 6
+}, {
+    name: "USDC",
+    mint: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    native: false,
+    price: '1',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1vAKYEl0YffTpWSxrqEi_gmUsl-0BuXSKMQ&s",
+    decimals: 6
+}]
