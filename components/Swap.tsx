5a77f2320169bfc946a6b883203fff6c8e32a7a4
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-23 00:08:23 +0530

    added swap functionality

--- components/Swap.tsx
+++ components/Swap.tsx
@@ -3,13 +3,20 @@
 import React, { useState } from 'react';
 import { SUPPORTED_TOKENS } from '@/lib/token';
 import { SwapAsset } from './SwapHelper';
+import { ArrowDownUp } from 'lucide-react';
 
 const Swap = () => {
   const [baseAsset, setBaseAsset] = useState(SUPPORTED_TOKENS[0]);
   const [quoteAsset, setQuoteAsset] = useState(SUPPORTED_TOKENS[2]);
 
+  const handleSwap = () => {
+    let newBaseAsset = baseAsset;
+    setBaseAsset(quoteAsset);
+    setQuoteAsset(newBaseAsset);
+  };
+
   return (
-    <div className='flex flex-col gap-y-5 mt-4 items-start'>
+    <div className='flex flex-col gap-y-3 mt-4 items-start'>
       {/* <div className='flex flex-col gap-y-1'>
         <span className='text-sm font-semibold'>You Pay:</span> */}
       <SwapAsset
@@ -21,6 +28,13 @@ const Swap = () => {
           {`Current Balance: 5641654 ${baseAsset.name}`}
         </span>
       </div> */}
+
+      <div
+        onClick={handleSwap}
+        className='flex items-center justify-center w-full cursor-pointer'
+      >
+        <ArrowDownUp className='size-4 text-slate-600 transform transition-transform duration-600 ease-in-out hover:rotate-180' />
+      </div>
 
       {/* <div className='flex flex-col gap-y-2'>
         <span className='text-sm font-semibold'>You Receive:</span> */}
