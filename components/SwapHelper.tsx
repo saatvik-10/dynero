c286df4536cc7972486bafdff9bb4b5f84557c5d
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-23 20:34:17 +0530

    added input

--- components/SwapHelper.tsx
+++ components/SwapHelper.tsx
@@ -4,22 +4,37 @@ import { SUPPORTED_TOKENS, TokenInterface } from '@/lib/token';
 
 export function SwapAsset({
   onSelect,
+  onAmountChange,
   selectedToken,
   title,
+  amount,
   totalBalance,
 }: {
   onSelect: (asset: TokenInterface) => void;
+  onAmountChange: (val: string) => void;
   selectedToken: TokenInterface;
   title: string;
+  amount?: string;
   totalBalance?: string;
 }) {
   return (
-    <div className='flex flex-col gap-y-1'>
-      <span className='text-sm font-semibold'>{title}</span>
-      <AssetSelection selectedToken={selectedToken} onSelect={onSelect} />
-      <span className='text-xs font-medium text-slate-500'>
-        {`Current Balance: ${totalBalance}`}
-      </span>
+    <div className='flex items-center justify-between w-full'>
+      <div className='flex flex-col gap-y-1'>
+        <span className='text-sm font-semibold'>{title}</span>
+        <AssetSelection selectedToken={selectedToken} onSelect={onSelect} />
+        <span className='text-xs font-medium text-slate-500'>
+          {`Current Balance: ${totalBalance}`}
+        </span>
+      </div>
+      <input
+        type='text'
+        placeholder='0'
+        className='w-20 outline-none text-center text-3xl'
+        value={amount}
+        onChange={(e) => {
+          onAmountChange(e.target.value);
+        }}
+      />
     </div>
   );
 }
@@ -32,7 +47,7 @@ export function AssetSelection({
   onSelect: (asset: TokenInterface) => void;
 }) {
   return (
-    <>
+    <div>
       <select
         onChange={(e) => {
           const selectedToken = SUPPORTED_TOKENS.find(
@@ -43,16 +58,14 @@ export function AssetSelection({
           }
         }}
         id='tokens'
-        className='bg-gray-50 text-gray-900 text-sm rounded-md block p-2.5'
+        className='bg-gray-50 text-gray-900 text-sm rounded-md block p-2.5 w-24'
       >
-        {SUPPORTED_TOKENS.map(
-          (tk, idx) => (
-            <option key={idx} selected={selectedToken.name === tk.name}>
-              {tk.name}
-            </option>
-          )
-        )}
+        {SUPPORTED_TOKENS.map((tk, idx) => (
+          <option key={idx} selected={selectedToken.name === tk.name}>
+            {tk.name}
+          </option>
+        ))}
       </select>
-    </>
+    </div>
   );
 }
