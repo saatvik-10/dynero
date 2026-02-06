8b19d678170e4b84ca749c90e720d091783ddf4b
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-06 17:41:07 +0530

    updated prisma configs

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,12 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import 'dotenv/config';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-//next js has hard reloading and we dont want to initialize prisma client multiple times
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: `${process.env.DATABASE_URL}`,
+});
 
-if (process.env.NODE_ENV !== "production") {
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = db;
 }
